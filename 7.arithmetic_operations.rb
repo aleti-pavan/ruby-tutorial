7bb62facea161331e8da5db53259fd6c71839579
Pavan Aleti <pavan.hpsa@gmail.com>
2022-03-28 19:54:41 +0100

    arithmetic operations

---
+++ 7.arithmetic_operations.rb
@@ -0,0 +1,33 @@
+# Arithmetic Operations
+
+=begin
+
+The interesting thing to note here is that, 
+since x was declared to be a float, the output will be a float as well. 
+If you want to get integer output, then you should declare both variables as integers.
+
+
+=end
+
+x = 5.0
+y = 3
+
+# addition
+sum = x + y
+puts "Sum of #{x} and #{y} is #{sum}"
+
+# subtraction
+sub = x - y
+puts "Substraction of #{x} and #{y} is #{sum}"
+
+# multiplication
+mul = x * y
+puts mul
+
+# division
+div = x / y
+puts div
+
+# mod
+mod = x % y
+puts mod
