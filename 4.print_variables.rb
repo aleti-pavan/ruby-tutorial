34cc54b2988c870c2fee48c7282aed9baf431705
Pavan Aleti <pavan.hpsa@gmail.com>
2022-03-28 19:40:26 +0100

    print vars

---
+++ 4.print_variables.rb
@@ -0,0 +1,13 @@
+# variable sum contains the sum of variable x and y
+x = 5
+y = 15
+sum = x + y
+puts sum
+puts "#{x}+#{y}= #{sum}"
+
+# variable data types
+name = "Pavan Aleti"
+age = 30
+puts "Your name is #{name} and you are #{age} years old"
+
+
