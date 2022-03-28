3f3c00b53f80c638bf563d317b000dcd7ee521ff
Pavan Aleti <pavan.hpsa@gmail.com>
2022-03-28 19:39:35 +0100

    variables

---
+++ 3.variables.rb
@@ -0,0 +1,15 @@
+=begin
+
+def	if	else	elsif	do
+next	then	retry	while	class
+rescue	when	in	self	module
+until	redo	return	case	or
+begin	end	super	unless	false
+break	for	defined?	alias	and
+
+=end
+
+first_var=10
+
+puts first_var #prints value of first_var i.e 10
+
