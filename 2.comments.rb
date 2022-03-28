ee65c682eb639220d1c48201acf51b0f55900aea
Pavan Aleti <pavan.hpsa@gmail.com>
2022-03-28 19:19:18 +0100

    comments

---
+++ 2.comments.rb
@@ -0,0 +1,12 @@
+# This is one line comment
+puts "Hello World"  #prints and adds a new line
+
+
+=begin
+    This is
+    multiline
+    comment
+=end
+
+print "Hello World" #prints and doesn't add a new line
+
