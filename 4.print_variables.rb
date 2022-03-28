76b751c891dfb64fbe094dfc477386c90339ab4c
Pavan Aleti <pavan.hpsa@gmail.com>
2022-03-28 19:41:40 +0100

    print vars

--- 4.print_variables.rb
+++ 4.print_variables.rb
@@ -3,7 +3,7 @@ x = 5
 y = 15
 sum = x + y
 puts sum
-puts "#{x}+#{y}= #{sum}"
+puts "#{x}+#{y}= #{sum}"  #you can insert the variable’s value into a string using # followed by the variable name inside curly braces.
 
 # variable data types
 name = "Pavan Aleti"
