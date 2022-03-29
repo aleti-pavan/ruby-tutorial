5e4d2e3fd074e25c2b4b243103f6d5d9c5f738b1
Pavan Aleti <pavan.hpsa@gmail.com>
2022-03-29 18:46:19 +0100

    iterators

---
+++ 2.ControlFlows and Collections/Iterators/1.times_iterator.rb
@@ -0,0 +1,10 @@
+
+#The times iterator is like a compact for loop and can perform the specified task on each item in the array.
+
+5.times do
+    puts "I am Learning Ruby!" 
+end
+
+5.times {
+    puts "I am Learning Ruby with {}!" 
+}
---
+++ 2.ControlFlows and Collections/Iterators/2.each.rb
@@ -0,0 +1,11 @@
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+days.each do |day| 
+    puts day 
+end
+puts "-------------------"
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July','August', 'September', 'October', 'November', 'December']
+
+months.each { |month| 
+    puts month 
+}
